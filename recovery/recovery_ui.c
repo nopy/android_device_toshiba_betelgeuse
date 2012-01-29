76764f0317f93b9430443d11ae04333c3d24b710
Artem Makhutov <artem@makhutov.org>
2012-01-29 13:06:52 +0100

    Some tests with recovery

--- recovery/recovery_ui.c
+++ recovery/recovery_ui.c
@@ -20,17 +20,6 @@
 #include "common.h"
 #include "extendedcommands.h"
 
-int device_toggle_display(volatile char* key_pressed, int key_code) {
-    int alt = key_pressed[KEY_POWER];
-    if (alt && key_code == KEY_VOLUMEUP)
-        return 1;
-    // allow toggling of the display if the correct key is pressed, and the display toggle is allowed or the display is currently off
-    if (ui_get_showing_back_button()) {
-        return get_allow_toggle_display() && (key_code == KEY_POWER && key_code == KEY_VOLUMEUP);
-    }
-    return get_allow_toggle_display() && (key_code == KEY_POWER && key_code == KEY_VOLUMEUP);
-}
-
 int device_handle_key(int key_code, int visible) {
     if (visible) {
         switch (key_code) {
