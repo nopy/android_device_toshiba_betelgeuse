e6ed645ac645a5d2c9eb97db665f2141219b6443
Artem Makhutov <artem@makhutov.org>
2012-04-29 23:11:49 +0200

    Some cleanup

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -2,7 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 TARGET_KERNEL_SOURCE := kernel/toshiba/betelgeuse
 TARGET_KERNEL_CONFIG := tegra_betelgeuse_android_defconfig
 
