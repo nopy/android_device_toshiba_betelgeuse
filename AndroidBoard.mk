93e8df5b487f15114f98c8281e260280403aed1b
Artem Makhutov <artem@makhutov.org>
2012-04-29 22:01:00 +0200

    Use kernel/toshiba/betelgeuse as kernel path

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
-TARGET_KERNEL_SOURCE := kernel/betelgeuse
+TARGET_KERNEL_SOURCE := kernel/toshiba/betelgeuse
 TARGET_KERNEL_CONFIG := tegra_betelgeuse_android_defconfig
 
 # include the non-open-source counterpart to this file
