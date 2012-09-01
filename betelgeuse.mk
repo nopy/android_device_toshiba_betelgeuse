5cfa5ad27a6f0786df291f75ec1fa617fd9dc17f
Artem Makhutov <artem@makhutov.org>
2012-09-01 12:13:13 +0200

    Fix bluetooth

--- betelgeuse.mk
+++ betelgeuse.mk
@@ -71,10 +71,8 @@ PRODUCT_COPY_FILES += \
 #    device/toshiba/betelgeuse/modules/bcm4329.ko:system/modules/bcm4329.ko 
 
 PRODUCT_COPY_FILES += \
-    hardware/AR6kSDK.3.1/AR6kSDK.build_3.1_RC.563/host/miscdrv/ar3k/30101coex/PS_ASIC_aclHighPri.pst:/system/wifi/ar3k/30101coex/PS_ASIC_aclHighPri.pst \
-    hardware/AR6kSDK.3.1/AR6kSDK.build_3.1_RC.563/host/miscdrv/ar3k/30101coex/PS_ASIC_aclLowPri.pst:/system/wifi/ar3k/30101coex/PS_ASIC_aclLowPri.pst \
-    hardware/AR6kSDK.3.1/AR6kSDK.build_3.1_RC.563/host/miscdrv/ar3k/30101coex/PS_ASIC.pst:/system/wifi/ar3k/30101coex/PS_ASIC.pst \
-    hardware/AR6kSDK.3.1/AR6kSDK.build_3.1_RC.563/host/miscdrv/ar3k/30101coex/RamPatch.txt:/system/wifi/ar3k/30101coex/RamPatch.txt
+    $(LOCAL_PATH)/wifi/ar3k/30101coex/PS_ASIC.pst:/system/wifi/ar3k/30101coex/PS_ASIC.pst \
+    $(LOCAL_PATH)/wifi/ar3k/30101coex/RamPatch.txt:/system/wifi/ar3k/30101coex/RamPatch.txt
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
